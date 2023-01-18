40d421d226209c1379e355c3990195361e4a4ef4
gibs0630 <122580207+gibs0630@users.noreply.github.com>
2023-01-17 19:52:14 -0800

    lab 0,1

---
+++ Gibson_Andrew_ECE351_Lab1.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+################################################################
+# #
+# Andrew Gibson #
+# ECE 351 lab, Section 53 #
+# Lab 0 #
+# Due 24 Jan 2023 #
+# Installation of Python 3.x #
+# #
+################################################################
+import numpy
+import scipy.signal
+import time
+
+t = 1
+print(t)
+print("t = ", t)
+print('t =',t,"seconds")
+print('t is now =',t/3,'\n...and can be rounded using "round()"', round(t/3,4))
+
+print (3**2)
+
+list1 = [0,1,2,3]
+print ('list1:', list1)
+list2 = [[0],[1],[2],[3]]
+print ('list2:', list2)
+list3 = [[0,1],[2,3]]
+print ('list3:', list3)
+array1 = numpy.array([1,2,3,4])
+print('array1:',array1)
+array2 = numpy.array([[0],[1],[2],[3]])
+print('array2:',array2)
+array3 = numpy.array([[0,1],[2,3]])
+print('array3:',array3)
---
+++ template_python.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+################################################################
+# #
+# Andrew Gibson #
+# ECE 351 lab, Section 53 #
+# Lab 0 #
+# Due 24 Jan 2023 #
+# Installation of Python 3.x #
+# #
+################################################################
+import numpy
+import matplotlib.pyplot
+import scipy
+import control
+import pandas
+
+print("hello world")
